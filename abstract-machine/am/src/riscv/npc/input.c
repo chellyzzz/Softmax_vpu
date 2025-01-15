bfcdd8d21aa06cef4051648f756c71f095c7cdce
xu zhi <xuzhi@node041.bosccluster.com>
2025-01-15 16:53:09 +0800

    add test

--- abstract-machine/am/src/riscv/npc/input.c
+++
@@ -1,9 +0,0 @@
-#include <am.h>
-#include <npc.h>
-
-#define KEYDOWN_MASK 0x8000
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  int k = inl(KBD_ADDR);
-  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
-  kbd->keycode = kbd->keydown ? (k & ~KEYDOWN_MASK) : AM_KEY_NONE;
-}
